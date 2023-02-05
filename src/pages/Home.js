6f5e2b497a7cdf23648bdb6aef0354dc1a3de9a7
cbowman422 <chrisb0wman@hotmail.com>
2023-02-04 19:54:26 -0500

    css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -17,8 +17,10 @@ signOut()
     <Rooms currentUser={currentUser} socket={socket} />
     
     <div className="homeComponent">
-    <h1 className="homeHeader"> Welcome to Millie's Messenger </h1>
-    <h2 className="homeBody"> The Live Public Chat Room is a place for everyone but the messages only stay as long as youre in there. You can also select a user chat room and send any user a prive message and these messages wont disappear. If you want to send a message to all prive users you can select your username from the header, this can also be used to set an away message. Millie's Messenger uses Greenwich Mean Time (GMT).</h2>
+    <h1 className="homeHeader"> Welcome to Millie's Messenger ! </h1>
+    <h2 className="homeBody"> Join the "Live Public Chat Room" or send a personal message to another user .
+    <br/> <br/> Click on your username to set a personalized away message . <br/> <br/> Millie's Messenger utilizes Greenwich Mean Time (GMT) .</h2>
+    <div> <br/> <br/> <h2 className="milSign"> - Mil. </h2> </div>
     </div>
     </div>
   )
